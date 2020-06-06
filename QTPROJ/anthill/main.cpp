7aa51bef1b97e73cfcfedbb53e2d357d22eedd70
Skaopap <b.thominet@orange.fr>
2020-06-06 09:18:39 +0200

    Update main.cpp

--- QTPROJ/anthill/main.cpp
+++ QTPROJ/anthill/main.cpp
@@ -6,41 +6,41 @@
 
 static constexpr int AntCount = 100;
 
-//! [0]
+
 int main(int argc, char **argv)
 {
     QApplication app(argc, argv);
-//! [0]
 
-//! [1]
+    // Set Scene
     QGraphicsScene scene;
     scene.setSceneRect(-300, -300, 600, 600);
-//! [1] //! [2]
     scene.setItemIndexMethod(QGraphicsScene::NoIndex);
-//! [2]
 
-//! [3]
-    for (int i = 0; i < AntCount; ++i) {
+    // Init ant object
+    for (int i = 0; i < AntCount; ++i)
+    {
         Ant *ant = new Warrior;
         ant->setPos(::sin((i * 6.28) / AntCount) * 200,
                       ::cos((i * 6.28) / AntCount) * 200);
         scene.addItem(ant);
     }
-//! [3]
 
-//! [4]
+    // Set View and background
     QGraphicsView view(&scene);
     view.setRenderHint(QPainter::Antialiasing);
     view.setBackgroundBrush(QPixmap(":/images/dirt.png"));
-//! [4] //! [5]
+
+    // Set QGraphicsView Mode
     view.setCacheMode(QGraphicsView::CacheBackground);
     view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
     view.setDragMode(QGraphicsView::ScrollHandDrag);
-//! [5] //! [6]
+
+    // Set Title and init window size
     view.setWindowTitle(QT_TRANSLATE_NOOP(QGraphicsView, "Sweaty Anthill"));
     view.resize(400, 300);
     view.show();
 
+    // Set FPS
     QTimer timer;
     QObject::connect(&timer, &QTimer::timeout, &scene, &QGraphicsScene::advance);
     timer.start(1000 / 33);
